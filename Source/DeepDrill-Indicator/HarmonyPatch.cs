849902bce7aeb122dfb110c565005a9b7014abf1
Georodin <georodin@gmx.de>
2023-09-17 01:30:56 +0200

    fixed display and selection
    - fixed display of resource amount
    - fixed display while in build mode with DeepDrill
    - fixed display when DeepDrill blueprint is selected
    - fixed display when minified DeepDrill is selected

--- Source/DeepDrill-Indicator/HarmonyPatch.cs
+++ Source/DeepDrill-Indicator/HarmonyPatch.cs
@@ -22,47 +22,124 @@ public class HarmonyPatch
 
     static bool Prefix(Map ___map, ushort[] ___defGrid)
     {
-        Thing singleSelectedThing = Find.Selector.SingleSelectedThing;
-        if (singleSelectedThing != null)
+        if (IsDeepMiningRelated(___map))
         {
-            CompDeepScanner compDeepScanner = singleSelectedThing.TryGetComp<CompDeepScanner>();
-            CompDeepDrill compDeepDrill = singleSelectedThing.TryGetComp<CompDeepDrill>();
-
-            if (((compDeepScanner != null || compDeepDrill != null)||singleSelectedThing.def.defName.Contains("autodrill")) && ___map.deepResourceGrid.AnyActiveDeepScannersOnMap())
+            int i = 0;
+            foreach (ushort entry in ___defGrid)
             {
-                int i = 0;
-                foreach (ushort entry in ___defGrid)
+                if (entry != 0)
                 {
-                    if (entry != 0)
+                    IntVec3 c = ___map.cellIndices.IndexToCell(i);
+                    if (!c.InBounds(___map))
                     {
-                        IntVec3 c = ___map.cellIndices.IndexToCell(i);
-                        if (!c.InBounds(___map))
+                        return true;
+                    }
+                    ThingDef thingDef = ___map.deepResourceGrid.ThingDefAt(c);
+                    if (thingDef != null)
+                    {
+                        int num = ___map.deepResourceGrid.CountAt(c);
+                        if (num > 0)
                         {
-                            return true;
-                        }
-                        ThingDef thingDef = ___map.deepResourceGrid.ThingDefAt(c);
-                        if (thingDef != null)
-                        {
-                            int num = ___map.deepResourceGrid.CountAt(c);
-                            if (num > 0)
-                            {
-                                Vector2 vector = c.ToVector3().MapToUIPosition();
+                            Vector2 vector = c.ToVector3().MapToUIPosition();
 
-                                float num2 = (UI.CurUICellSize() - DeepDrill_Indicator.Settings.sliderValueDDI) / 2f;
-                                Rect rect = new Rect(vector.x + num2, vector.y - UI.CurUICellSize() + num2, DeepDrill_Indicator.Settings.sliderValueDDI, DeepDrill_Indicator.Settings.sliderValueDDI);
-                                Widgets.ThingIcon(rect, thingDef);
-                                if (Find.CameraDriver.CurrentZoom==CameraZoomRange.Closest) {
-                                    GUI.color = Color.white;
-                                    Text.Font = GameFont.Small;
-                                    Text.Anchor = TextAnchor.MiddleLeft;
-                                    Widgets.Label(new Rect(rect.xMax + 4f, rect.y, 999f, 29f), num+"");
-                                    Text.Anchor = TextAnchor.UpperLeft;
-                                }
+                            float num2 = (UI.CurUICellSize() - DeepDrill_Indicator.Settings.sliderValueDDI) / 2f;
+                            Rect rect = new Rect(vector.x + num2, vector.y - UI.CurUICellSize() + num2, DeepDrill_Indicator.Settings.sliderValueDDI, DeepDrill_Indicator.Settings.sliderValueDDI);
+
+                            Widgets.ThingIcon(rect, thingDef);
+
+                            if (Find.CameraDriver.CurrentZoom == CameraZoomRange.Closest)
+                            {
+                                GUI.color = Color.white;
+                                Text.Font = GameFont.Small;
+                                Text.Anchor = TextAnchor.MiddleCenter;
+
+                                Widgets.Label(new Rect(rect.x + (rect.width / 2f) - 15f, rect.y, 30f, 29f), num.ToString());
+
+                                Text.Anchor = TextAnchor.UpperLeft; // Resetting the text anchor
                             }
                         }
                     }
-                    i++;
                 }
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    static Thing last_selected_thing = null;
+    static Designator last_selected_designator = null;
+    static BuildableDef last_selected_buildable = null;
+
+    static bool IsDeepMiningRelated(Map ___map)
+    {
+        //is Scanner running?
+        if (!___map.deepResourceGrid.AnyActiveDeepScannersOnMap())
+        {
+            return false;
+        }
+
+        if (IsSelectedDeepMiningRelated())
+        {
+            return true;
+        }
+
+        if (IsDesignatorDeepMiningRelated())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static bool IsSelectedDeepMiningRelated()
+    {
+        Thing singleSelectedThing = Find.Selector.SingleSelectedThing;
+
+        //check for Scanner
+        if (singleSelectedThing.TryGetComp<CompDeepScanner>() != null)
+        {
+            return true;
+        }
+
+        //check for Drill
+        if (singleSelectedThing.TryGetComp<CompDeepDrill>() != null)
+        {
+            return true;
+        }
+
+        //check for minified Drill
+        if (singleSelectedThing is MinifiedThing minified)
+        {
+            if (minified.InnerThing.TryGetComp<CompDeepDrill>() != null)
+            {
+                return true;
+            }
+        }
+
+        // Check for DeepDrill blueprint
+        if (singleSelectedThing is Blueprint_Build blueprint)
+        {
+            if (blueprint.def.entityDefToBuild == ThingDefOf.DeepDrill)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsDesignatorDeepMiningRelated()
+    {
+        //check if drill is selected as building blueprint
+        Designator currentDesignator = Find.DesignatorManager.SelectedDesignator;
+        if (currentDesignator is Designator_Build designatorBuild)
+        {
+            BuildableDef buildable = designatorBuild.PlacingDef as BuildableDef;
+
+            if (buildable != null && buildable is ThingDef thingDef && thingDef == ThingDefOf.DeepDrill)
+            {
+                return true;
             }
         }
 
